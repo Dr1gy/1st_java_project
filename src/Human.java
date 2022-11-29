bd2c264577b9e711924c21cb22f198f469aa227b
Dr1gy <yastremskiynik@gmail.com>
2022-11-29 11:48:59 +0300

    код нахождения максимума

--- src/Human.java
+++
@@ -1,5 +0,0 @@
-public class Human {
-    public int age;
-    public double weight;
-    public double height;
-}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        Human humanDima = new Human();
+        int x1 = 4, x2 = 3, x3 = 2, x4 = 1;
+        int m1 = Math.max(x1, x2), m2 = Math.max(x3, x4);
+        int m = Math.max(m1, m2);
+        System.out.println(m);
     }
 }
